aea504f2daa2ab5db48272b3358f488527210ac4
Rasmus3liasson <ras.muse@hotmail.com>
2024-04-22 12:00:55 +0200

    Removed Education component

--- src/app/components/Containers/page.tsx
+++ src/app/components/Containers/page.tsx
@@ -1,12 +1,10 @@
-import React from "react";
 import Knowledge from "./Knowledge";
-import Education from "./Education";
 
 export default function Containers() {
   return (
     <>
       <Knowledge />
-      <Education />
+      {/* <Education /> */}
     </>
   );
 }
