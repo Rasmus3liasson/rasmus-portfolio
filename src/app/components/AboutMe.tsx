652ebf9f3e00f7ea4b58b8a3493d0a27e203e625
Rasmus3liasson <ras.muse@hotmail.com>
2024-07-13 11:42:04 +0200

    Changed information about me

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -1,5 +1,7 @@
-
 export default function AboutMe() {
+  const text = `Har under de senaste åren utvecklat ett stort intresse för utveckling. Jag har ägnat mycket tid åt att lära mig nya teknologier och förbättra mina färdigheter inom programmering och mjukvaruutveckling. Det har varit en spännande resa som har gett mig både teoretisk kunskap och praktisk erfarenhet genom utbildningar/praktiker och projekt som jag skapat.<br /><br />
+  Jag söker nu nya möjligheter inom detta område och är mycket intresserad av att hitta en roll där jag kan bidra med mina kunskaper och fortsätta växa professionellt. Om du eller ditt företag söker någon med passion för utveckling och en stark vilja att lära och växa, skulle jag vara mycket glad att diskutera möjliga samarbeten eller jobbalternativ.`;
+
   return (
     <>
       <div id="aboutMe"></div>
@@ -7,16 +9,7 @@ export default function AboutMe() {
         <div className="text-colors-white p-9 text-center">
           <h3 className="text-3xl">Hejsan, mitt namn är Rasmus Eliasson</h3>
           <div>
-            <p className="mt-6 leading-7 mb-20 lg:text-xl max-w-3xl">
-              Har under det senaste året viljat ta en vändnig i min yrkesbana
-              till att jobba inom IT. <br />
-              Jag har sedan studenten jobbat på postkontoret i min hemstad med
-              paket och brev. Men efter dessa fem åren så känner jag en strävan
-              efter att byta branch i min yrkeskarriär. <br />
-              Jag läser i dagsläget en 2 årig utbildning på yrkeshögskola som
-              bedrivs på distans av Lernia. <br />
-              Fokus i utbildnignen ligger i Javascript samt Java.
-            </p>
+            <p className="mt-6 leading-7 mb-20 lg:text-xl max-w-3xl" dangerouslySetInnerHTML={{ __html: text }}></p>
           </div>
         </div>
       </article>
